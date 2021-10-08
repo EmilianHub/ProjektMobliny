f5b71cba54f5a3c771977bae4fa2e282582f5895
Emilian Kochanek <ek9164@student.pwsz-ns.edu.pl>
2021-10-08 12:34:21 +0200

    Dodaj pliki projektów.

---
+++ ProjektMobliny/Models/Item.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ProjektMobliny.Models
+{
+    public class Item
+    {
+        public string Id { get; set; }
+        public string Text { get; set; }
+        public string Description { get; set; }
+        public DateTime FinishedDate { get; set; }
+    }
+}
