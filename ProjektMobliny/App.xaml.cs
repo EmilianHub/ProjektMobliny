7d2dba76a32bb25b79e22db55964561a4045b4aa
Emilian Kochanek <ek9164@student.pwsz-ns.edu.pl>
2021-10-08 12:34:21 +0200

    Dodanie geolokacji

---
+++ ProjektMobliny/App.xaml.cs
@@ -0,0 +1,54 @@
+﻿using ProjektMobliny.Services;
+using ProjektMobliny.Views;
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using Xamarin.Essentials;
+
+namespace ProjektMobliny
+{
+    public partial class App : Application
+    {
+
+        public App()
+        {
+            InitializeComponent();
+
+            DependencyService.Register<MockDataStore>();
+            MainPage = new AppShell();
+        }
+
+        private async void Button_Clicked(Object sender, EventArgs e)
+        {
+            try
+            {
+                var lokacja = await Geolocation.GetLastKnownLocationAsync();
+                if(lokacja == null)
+                {
+                    lokacja = await Geolocation.GetLocationAsync(new GeolocationRequest 
+                    { 
+                        DesiredAccuracy = GeolocationAccuracy.Medium, 
+                        Timeout = TimeSpan.FromSeconds(45) 
+                    }); 
+                }
+
+            }
+            catch(Exception ew)
+            {
+                 Console.WriteLine($"Coś poszło nie tak {ew.Message}");
+            }
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
