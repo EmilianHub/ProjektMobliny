1df7ce91439e81335f7a2a5da22b0f762902b3cf
Emilian Kochanek <ek9164@student.pwsz-ns.edu.pl>
2021-10-09 20:40:35 +0200

    Layout

--- ProjektMobliny/Views/MainPage.xaml.cs
+++ ProjektMobliny/Views/MainPage.xaml.cs
@@ -9,11 +9,7 @@ using Xamarin.Forms.Xaml;
 namespace ProjektMobliny.Views
 {
     public partial class MainPage : ContentPage
-    {
-        private Picker wybor;
-        private Button przycisk;
-        public Entry wejscie;
-        
+    {              
         public MainPage()
         {
             InitializeComponent();
@@ -23,20 +19,16 @@ namespace ProjektMobliny.Views
             Stacja.Add(new Stacje() { Id = 3, Nazwa = "BP" });
             Stacja.Add(new Stacje() { Id = 4, Nazwa = "Lotos" });
             Stacja.Add(new Stacje() { Id = 5, Nazwa = "Circle" });
-            StackLayout stackLayout = new StackLayout();         
-                wybor = new Picker();
-                wybor.Title = "Wybierz stacje paliw";
-                wybor.ItemsSource = Stacja;
-                stackLayout.Children.Add(wybor);
-                przycisk = new Button();
-                przycisk.Text = "Szukaj stacji";
-                przycisk.Clicked += Przycisk_Clicked;
-                stackLayout.Children.Add(przycisk);
+            wybor.Title = "Wybierz stacje";
+            wybor.ItemsSource = Stacja;
+            Glowna.Children.Add(wybor);
+            przycisk.Text = "Szukaj stacji";
+            przycisk.Clicked += Przycisk_Clicked;
+            Glowna.Children.Add(przycisk);
+            wejscie.Keyboard = Keyboard.Text;
+            wejscie.Placeholder = "Wybrana stacja";
+            Glowna.Children.Add(wejscie);
 
-                wejscie = new Entry();
-                wejscie.Keyboard = Keyboard.Text;
-                wejscie.Placeholder = "Wybrana stacja";
-                stackLayout.Children.Add(wejscie);
         }
 
         public void Przycisk_Clicked(object sender, EventArgs e)
