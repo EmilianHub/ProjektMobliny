75644312e6471bf33dbb44c8b0e25ed44aae0890
Norbert Lachner <norbertlachner1@gmail.com>
2022-01-11 14:45:51 +0100

    Tablice

---
+++ ProjektMobliny/Models/adres.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProjektMobliny.Models
+{
+    public class adres
+    {
+        public string marka { get; set; }
+        public double szerokosc { get; set; }
+        public double dlugosc { get; set; }
+    }
+}
